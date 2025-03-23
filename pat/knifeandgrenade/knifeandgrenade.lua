5a3d10b40028dc056587a01bd2fa49ec4c88ae23
patman <27874300+patmanf@users.noreply.github.com>
2025-03-23 12:17:32 -0500

    rewrite the entire item

--- pat/knifeandgrenade/knifeandgrenade.lua
+++ pat/knifeandgrenade/knifeandgrenade.lua
@@ -1,57 +1,81 @@
-require "/scripts/util.lua"
 require "/scripts/vec2.lua"
-require "/scripts/interp.lua"
 
--- Base gun fire ability
-GunFire = WeaponAbility:new()
+function init()
+  local get = config.getParameter
 
-function GunFire:init()
-  self.weapon:setStance(self.stances.idle)
+  self.projectileTypes = get("projectileTypes")
+  self.projectileCount = get("projectileCount", 1)
+  self.spawnHeight = get("spawnHeight", 50)
+  self.spawnRadius = get("spawnRadius", 1)
+  self.speedRange = get("speedRange")
+  self.inaccuracy = get("inaccuracy")
+  self.aimVerticalOffset = get("aimVerticalOffset", 0)
 
-  self.cooldownTimer = 0
+  self.cooldownTime = get("cooldownTime")
+  self.cooldown = 0
 
-  self.weapon.onLeaveAbility = function()
-    self.weapon:setStance(self.stances.idle)
+  self.cursors = get("cursors")
+  activeItem.setCursor(self.cursors.idle)
+
+  self.ownerId = activeItem.ownerEntityId()
+end
+
+function update(dt, fireMode)
+  local firing = fireMode ~= "none"
+
+  local aimPos = activeItem.ownerAimPosition()
+  local angle, dir = activeItem.aimAngleAndDirection(self.aimVerticalOffset, aimPos)
+
+  activeItem.setArmAngle(angle)
+  activeItem.setFacingDirection(firing and dir or 0)
+  activeItem.setHoldingItem(firing)
+  activeItem.setCursor(firing and self.cursors.firing or self.cursors.idle)
+
+  if self.cooldown > 0 then
+    self.cooldown = math.max(0, self.cooldown - dt)
+  end
+
+  if firing and self.cooldown == 0 then
+    fire()
+    self.cooldown = self.cooldownTime
   end
 end
 
-function GunFire:update(dt, fireMode, shiftHeld)
-  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+function fire()
+  local mPos = mcontroller.position()
+  local centerPos = {mPos[1], mPos[2] + self.spawnHeight}
 
-  self.cooldownTimer = math.max(0, self.cooldownTimer - self.dt)
+  local aimPos = activeItem.ownerAimPosition()
+  local aimAngle = math.atan(aimPos[2] - centerPos[2], aimPos[1] - centerPos[1])
 
-  if self.fireMode == "primary"
-  and self.cooldownTimer == 0
-  and not self.weapon.currentAbility
-  and not status.resourceLocked("energy") then
-		self:setState(self.fire)
+  local params = {}
+  params.powerMultiplier = activeItem.ownerPowerMultiplier()
+
+  for i = 1, self.projectileCount do
+    local spawnPos = randomInRadius(centerPos, self.spawnRadius)
+
+    local angle = aimAngle
+    if self.inaccuracy then angle = angle + sb.nrand(self.inaccuracy, 0) end
+    local aimVector = {math.cos(angle), math.sin(angle)}
+
+    if self.speedRange then
+      params.speed = randomInRange(self.speedRange)
+    end
+
+    local pType = self.projectileTypes[math.random(#self.projectileTypes)]
+    world.spawnProjectile(pType, spawnPos, self.ownerId, aimVector, false, params)
   end
 end
 
-function GunFire:fire()
-	local pos = vec2.add(mcontroller.position(), {0, 50})
-	local apos = activeItem.ownerAimPosition()
-	
-	local rot = math.atan(apos[2] - pos[2], apos[1] - pos[1])
-	
-	local pos1 = vec2.add(pos, {2, 0})
-	local pos2 = vec2.add(pos, {-2, 0})
-	
-	local params = {}
-	
-	for i = 1, 2 do
-		world.spawnProjectile(
-			self.projectiles[math.random(#self.projectiles)],
-			vec2.lerp(math.random(), pos1, pos2),
-			activeItem.ownerEntityId(),
-			vec2.rotate({1, 0}, rot),
-			false,
-			params
-		)
-	end
-	
-	self.cooldownTimer = 0.05
+function randomInRadius(center, radius)
+  local r = radius * math.sqrt(math.random())
+  local t = math.random() * 2 * math.pi
+  return {
+    center[1] + r * math.cos(t),
+    center[2] + r * math.sin(t)
+  }
 end
 
-function GunFire:uninit()
+function randomInRange(range)
+  return range[1] + (math.random() * (range[2] - range[1]))
 end
